7f16b88ce6ab22a9abda12bad83c1c15d1678931
Brandon Carlos <brandon_marzo@hotmail.com>
2024-08-26 19:14:49 -0300

    Application module adding Controller Advice and implement Input ports

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/service/OrderApplicationService.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/service/OrderApplicationService.java
@@ -8,7 +8,7 @@ import com.food.ordering.system.order.service.domain.dto.track.TrackOrderRespons
 import javax.validation.Valid;
 
 //How go to use this class? Client
-public interface OrderApplicationService {// We will go declare same methods over here because Domain layer that will go to implement these methods over here
+public interface OrderApplicationService {// We will go declare same methods over here because Domain layer(order-application) that will go to implement these methods over here
 
     CreateOrderResponse createOrder(@Valid CreateOrderCommand createOrderCommand);//Valid annotation that is for enabled the annotation there are in the CreateOrderCommand
 
