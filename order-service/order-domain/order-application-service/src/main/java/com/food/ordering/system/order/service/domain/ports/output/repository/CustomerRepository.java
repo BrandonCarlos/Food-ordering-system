0224e699533f0763c77f6674a1ce16bb7883906d
Brandon Carlos <brandon_marzo@hotmail.com>
2024-08-27 14:35:17 -0300

    Data access module Adding Customer and Restaurant Implementations

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/CustomerRepository.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.food.ordering.system.order.service.domain.ports.output.repository;
 
-import com.food.ordering.system.domain.valueobject.CustomerId;
 import com.food.ordering.system.order.service.domain.entity.Customer;
 
 import java.util.Optional;
